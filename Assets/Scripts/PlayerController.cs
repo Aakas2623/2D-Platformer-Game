2aa22fda2d92a0b89e363ccd302ebac7b9c1bc23
Aakash Singh <aaka241@gmail.com>
2024-10-29 07:12:33 +0530

    added player input

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,7 +1,8 @@
 using UnityEngine;
 
-public class NewMonoBehaviourScript : MonoBehaviour
+public class PlayerController : MonoBehaviour
 {
+    public Animator animator;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -9,8 +10,19 @@ public class NewMonoBehaviourScript : MonoBehaviour
     }
 
     // Update is called once per frame
-    void Update()
+    private void Update()
     {
-        
+        float Speed = Input.GetAxisRaw("Horizontal");
+        animator.SetFloat("Speed", Mathf.Abs(Speed));
+
+        Vector3 scale = transform.localScale;
+        if (Speed  < 0)
+        {
+            scale.x = -1f* Mathf.Abs(scale.x);
+        }
+        else if (Speed > 0) {
+            scale.x = Mathf.Abs(scale.x);
+        }
+        transform.localScale = scale;
     }
 }
